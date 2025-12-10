dd4884dd6f914b29e840a7c63c2240c0699b0422
Jujaru <2440947@cognizant.com>
2025-12-10 18:14:26 +0530

    Resolved conflict in cart count

--- src/app/core/header/header.ts
+++ src/app/core/header/header.ts
@@ -1,28 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { CommonModule } from '@angular/common';
 import { UserLogin } from '../../user-module/user-login/user-login';
-
+import { CommonModule } from '@angular/common';
+import { CartService } from '../../order-module/Services/cart-service';
 
 @Component({
   selector: 'app-header',
-  standalone: true,
   imports: [RouterLink, CommonModule],
   templateUrl: './header.html',
   styleUrls: ['./header.css']
 })
 export class Header {
   cartCount = 0;
-
+  constructor(private cartService: CartService) {
+    this.cartService.cartCount$.subscribe(count => {
+      this.cartCount = count;
+    });
+  }
   get isLoggedIn(): boolean {
+    // Always check localStorage so state persists across refresh
     return localStorage.getItem('isLoggedIn') === 'true';
   }
-  get brandLink(): string {
-    return this.isLoggedIn ? '/restaurantlist' : '/';
-  }
+
   logout() {
     UserLogin.isLoggedIn = false;
-    localStorage.setItem('isLoggedIn', 'false'); 
+    localStorage.removeItem('isLoggedIn'); // clear login state
     console.log('User logged out');
   }
 }
